fdcb7c825c8e9063591542bc31eebe4101f243f1
talhazaryab <talhazaryabmail@gmail.com>
2023-03-14 01:33:10 +0500

    layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,24 +2,40 @@ import React from "react";
 
 function Layout() {
     return (
-        <div className="flex text-white  h-screen">
-            <div className="bg-gray-800 p-3 space-y-2 overflow-y-scroll">
+        <div className="flex h-screen text-white">
+            <div className="p-3 bg-gray-700">
+                <div className="flex justify-center w-12 h-12 bg-white item-center">
+                    HW
+                </div>
+            </div>
+            <div className="flex flex-col bg-gray-600 w-60">
+                <div className="p-4 shadow-md">Tailwind CSS</div>
+                <div className="flex-1 p-4">Channels</div>
+            </div>
+            <div className="flex-1 bg-gray-500">Message</div>
+        </div>
+    );
+}
+
+/**
+ *
+ *  <div className="flex h-screen text-white">
+            <div className="p-3 space-y-2 overflow-y-scroll bg-gray-800">
                 {[...Array(30)].map((_, i) => (
-                    <div className="bg-white w-12 h-12 flex justify-center items-center text-gray-500 rounded-full">
+                    <div className="flex items-center justify-center w-12 h-12 text-gray-500 bg-white rounded-full">
                         {i}
                     </div>
                 ))}
             </div>
-            <div className="bg-gray-700 w-60 flex flex-col">
+            <div className="flex flex-col bg-gray-700 w-60">
                 <div className="p-4 shadow-md">Tailwind CSS</div>
-                <div className="p-4 flex-1">Channels</div>
+                <div className="flex-1 p-4">Channels</div>
             </div>
-            <div className="bg-gray-600 flex-1  flex flex-col">
+            <div className="flex flex-col flex-1 bg-gray-600">
                 <div className="p-4 shadow-md">General</div>
-                <div className="p-4 flex-1">Messages</div>
+                <div className="flex-1 p-4">Messages</div>
             </div>
         </div>
-    );
-}
+ */
 
 export default Layout;
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,20 +3,20 @@ import React from "react";
 function Message() {
     let src = "/profile-pic.png";
     return (
-        <div className="bg-gray-700 flex justify-center items-center text-white min-h-screen">
+        <div className="flex items-center justify-center min-h-screen text-white bg-gray-700">
             <div className="max-w-lg">
-                <div className="flex hover:bg-gray-800 hover:bg-opacity-30 px-4 py-1">
+                <div className="flex px-4 py-1 hover:bg-gray-800 hover:bg-opacity-30">
                     <img
                         src={src}
-                        className="w-10 mr-4 rounded-full h-10"
+                        className="w-10 h-10 mr-4 rounded-full"
                         alt=""
                     />
                     <div>
                         <p className="flex items-baseline">
-                            <span className="text-green-500 mr-2 text-sm  font-medium">
+                            <span className="mr-2 text-sm font-medium text-green-500">
                                 talhazaryab
                             </span>
-                            <span className="text-xs   text-gray-500">
+                            <span className="text-xs text-gray-500">
                                 10/03/2023
                             </span>
                         </p>
@@ -29,14 +29,14 @@ function Message() {
                     </div>
                 </div>
 
-                <div className="mt-1  hover:bg-gray-800 hover:bg-opacity-30 px-4 py-1">
+                <div className="px-4 py-1 mt-1 hover:bg-gray-800 hover:bg-opacity-30">
                     <p className="text-gray-300 pl-14">
                         Lorem ipsum dolor sit amet, consectetur adipisicing
                         elit. Repellat necessitatibus animi eius assumenda quo
                         perspiciatis, molestias deleniti id atque quibusdam.
                     </p>
                 </div>
-                <div className="mt-1  hover:bg-gray-800 hover:bg-opacity-30 px-4 py-1">
+                <div className="px-4 py-1 my-3 mt-1 hover:bg-gray-800 hover:bg-opacity-30">
                     <p className="text-gray-300 pl-14">
                         Lorem ipsum dolor sit amet, consectetur adipisicing
                         elit. Repellat necessitatibus animi eius assumenda quo
